e51e2682f9fd1313982ef6c4cb83e90111704568
kibernetik542 <qwerty5798858@box.az>
2017-05-12 23:35:56 +0400

    underscoreja with notifications

--- Concert/App_Start/BundleConfig.cs
+++ Concert/App_Start/BundleConfig.cs
@@ -8,7 +8,9 @@ namespace Concert
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/underscore-min.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -26,6 +28,8 @@ namespace Concert
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/animate.css",
+                      "~/Content/font-awesome.css",
                       "~/Content/site.css"));
         }
     }
